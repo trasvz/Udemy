ba05c27a29b882309ebd686c7d3f840ad1eccebc
trasvz <muhammadfatih4899@gmail.com>
2023-10-23 20:46:31 +0700

    add unit test and fix math js

--- Testing Course/basic-testing-frontend/src/math.test.js
+++ Testing Course/basic-testing-frontend/src/math.test.js
@@ -10,3 +10,20 @@ it('should summarize all number values in an array', () => {
 
     expect(actualResult).toEqual(expectedResult);
 });
+
+it('should return NaN if at least one of number in an array is invalid number', () => {
+    const arrayToSummarize = ['string', 2];
+
+    const actualResult = add(arrayToSummarize);
+
+    expect(actualResult).toBeNaN();
+});
+
+it('should return a correct sum if an array of numeric string values is provided', () => {
+    const arrayToSummarize = ['1', '2', '3'];
+    const expectedResult = 6;
+
+    const actualResult = add(arrayToSummarize);
+
+    expect(actualResult).toEqual(expectedResult);
+});
