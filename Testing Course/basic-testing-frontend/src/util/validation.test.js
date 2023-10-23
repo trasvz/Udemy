d8e43aef70961c51519e8ff3e111287b5efc1688
trasvz <muhammadfatih4899@gmail.com>
2023-10-23 21:34:22 +0700

    adding logic to validation js and add unit test to it

---
+++ Testing Course/basic-testing-frontend/src/util/validation.test.js
@@ -0,0 +1,57 @@
+import { it, expect } from 'vitest';
+
+import { validateStringNotEmpty, validateNumber } from './validation';
+
+it('should not throw error when not empty string is provided', () => {
+    const input = 'string';
+
+    const triggerFunction = () => { validateStringNotEmpty(input); };
+
+    expect(triggerFunction).not.toThrow();
+});
+
+it('should throw error when empty string is provided', () => {
+    const input = '';
+
+    const triggerFunction = () => { validateStringNotEmpty(input); };
+
+    expect(triggerFunction).toThrow();
+});
+
+it('should throw error when any other value than a string is provided', () => {
+    const numericInput = 1;
+    const booleanInput = true;
+    const objectInput = {};
+
+    const triggerFunctionNumeric = () => { validateStringNotEmpty(numericInput); };
+    const triggerFunctionBoolean = () => { validateStringNotEmpty(booleanInput); };
+    const triggerFunctionObject = () => { validateStringNotEmpty(objectInput); };
+
+    expect(triggerFunctionNumeric).toThrow();
+    expect(triggerFunctionBoolean).toThrow();
+    expect(triggerFunctionObject).toThrow();
+});
+
+it('should not throw error when actual number is provided', () => {
+    const input = 1;
+
+    const triggerFunction = () => { validateNumber(input); };
+
+    expect(triggerFunction).not.toThrow();
+});
+
+it('should throw error when string with non numeric values is provided', () => {
+    const input = 'string';
+
+    const triggerFunction = () => { validateNumber(input); };
+
+    expect(triggerFunction).toThrow();
+});
+
+it('should throw error when string with numeric values is provided', () => {
+    const input = '1';
+
+    const triggerFunction = () => { validateNumber(input); };
+
+    expect(triggerFunction).toThrow();
+});
