93d24b181a00660c944e1c832bde24cd1ab65bc0
trasvz <muhammadfatih4899@gmail.com>
2023-10-23 21:02:24 +0700

    add more unit test

--- Testing Course/basic-testing-frontend/src/math.test.js
+++ Testing Course/basic-testing-frontend/src/math.test.js
@@ -27,3 +27,26 @@ it('should return a correct sum if an array of numeric string values is provided
 
     expect(actualResult).toEqual(expectedResult);
 });
+
+it('should return 0 if empty array is provided', () => {
+    const arrayToSummarize = [];
+    const expectedResult = 0;
+
+    const actualResult = add(arrayToSummarize);
+
+    expect(actualResult).toBe(expectedResult);
+});
+
+it('should throw error if no value is provided', () => {
+    const resultFunction = () => { add(); };
+
+    expect(resultFunction).toThrow()
+});
+
+it('should throw an error if input is multiple argumeents instead of an array', () => {
+    const firstArgument = 1;
+    const secondArgument = 2;
+    const resultFunction = () => { add(firstArgument, secondArgument); };
+    
+    expect(resultFunction).toThrow()
+});
