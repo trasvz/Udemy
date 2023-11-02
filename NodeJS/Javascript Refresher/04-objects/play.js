728124d651376ca6acabfc030b8fe6090d30b1bb
trasvz <muhammadfatih4899@gmail.com>
2023-11-02 15:09:00 +0700

    objects and functions inside an object in js

---
+++ NodeJS/Javascript Refresher/04-objects/play.js
@@ -0,0 +1,12 @@
+const person = {
+  name: 'Max',
+  age: 29,
+  // greet = () => {
+  //   console.log('Hi, I am ' + this.name);
+  // }
+  greet() {
+    console.log('Hi, I am ' + this.name);
+  }
+};
+
+person.greet();
