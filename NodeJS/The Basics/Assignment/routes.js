2ae3df08c8bdbe4756fae4e90acd8dc86d6c4784
trasvz <muhammadfatih4899@gmail.com>
2023-11-12 18:26:30 +0700

    assignment - the basics

---
+++ NodeJS/The Basics/Assignment/routes.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Hello</title></head>');
+    res.write('<head><h1>Hello this is dummy text</h1></head>');
+    res.write(
+      '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/users') {
+    res.write('<html>');
+    res.write('<head><ul><li>users1</li><li>users2</li></ul></head>');
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/create-user' && method === 'POST') {
+    const body = [];
+    req.on('data', chunk => {
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      return res.end();
+    });
+  }
+};
+
+module.exports.handler = requestHandler;
