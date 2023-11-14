99b46ed47f2fc476112cc5b0852c1c644088f77c
trasvz <muhammadfatih4899@gmail.com>
2023-11-15 01:09:37 +0700

    introducing to middleware

--- NodeJS/Working with ExpressJs/app.js
+++ NodeJS/Working with ExpressJs/app.js
@@ -3,6 +3,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('in the middleware');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('in another middleware');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000)
